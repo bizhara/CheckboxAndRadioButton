448416b852b9db1cdffe264de181de9d67479f3d
Kazuaki Hara <bizhara@gmail.com>
2014-08-08 19:22:32 +0900

    初期プロジェクト登録

---
+++ CheckboxAndRadioButton/CheckboxAndRadioButton/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CheckboxAndRadioButton
+//
+//  Created by hara on 2014/08/08.
+//  Copyright (c) 2014年 Kazuaki Hara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
